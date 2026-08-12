c2560930cd4fed446bb08bc5e9184691c7a1dbe0
Manuel Cau <manuel.cau106@gmail.com>
2026-08-12 16:16:29 +0200

    color button hover

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -16,7 +16,7 @@ export function Button({
   const base = "px-4 py-2 rounded-lg text-sm font-medium transition";
   const variants = {
     default: "bg-gray-400 text-gray-200 hover:bg-gray-200",
-    primary: "bg-blue-400 text-white hover:bg-teal-700",
+    primary: "bg-blue-400 text-white hover:bg-blue-600",
   };
 
   return (
