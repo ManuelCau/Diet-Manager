e5da771e1d83879c17f1153991c16421498b18c0
Manuel Cau <manuel.cau106@gmail.com>
2026-08-12 16:27:13 +0200

    fix: problems w viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/nome-repo/",
+  base: "/Diet-Manager/",
   test: {
     environment: "jsdom",
     setupFiles: "./src/test/setup.ts",
